6866c8286bab95d665658bbebddfb7c1af6e5e09
H-chai <143179912+H-chai@users.noreply.github.com>
2025-06-01 00:04:40 +0900

    Adjust footer contact font size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,20 +6,20 @@ import { Link } from 'react-router-dom';
 export function Footer() {
   const isMobile = useMediaQuery('(max-width: 1023px)');
   return (
-    <footer className="bg-light-green font-clash text-dark-green px-4 pt-16 lg:px-16">
+    <footer className="bg-light-green font-clash text-dark-green px-4 pt-16 lg:px-16 lg:pt-28">
       <div className="text-dark-green bg-light-green font-clash pb-12 sm:pb-16 lg:pb-20">
         <Link
           to="mailto:overby.hikari@gmail.com"
-          className="flex items-center justify-between bg-yellow rounded-full cursor-pointer group mx-auto w-fit px-4 py-2 gap-2 sm:px-6 sm:gap-3 lg:px-10 lg:gap-6"
+          className="flex items-center justify-between bg-yellow rounded-full cursor-pointer group mx-auto w-fit px-4 py-2 gap-2 sm:px-6 sm:gap-3 lg:px-10 lg:gap-6 lg:py-0"
         >
           <span className="overflow-hidden">
-            <h1 className="font-medium text-[8vw] uppercase text-slide whitespace-nowrap lg:text-[7vw]">
+            <h1 className="font-medium text-[8vw] uppercase text-slide whitespace-nowrap lg:text-[6vw]">
               Get in touch
             </h1>
           </span>
 
-          <span className="w-[10vw] h-[10vw] bg-dark-green flex items-center justify-center rounded-full sm:w-[9vw] sm:h-[9vw] lg:w-[8vw] lg:h-[8vw]">
-            <EmailOutlinedIcon className="text-yellow !w-[6vw] !h-[6vw] md:!w-[5vw] md:!h-[5vw]" />
+          <span className="w-[10vw] h-[10vw] bg-dark-green flex items-center justify-center rounded-full sm:w-[9vw] sm:h-[9vw] lg:w-[7vw] lg:h-[7vw]">
+            <EmailOutlinedIcon className="text-yellow !w-[6vw] !h-[6vw] md:!w-[5vw] md:!h-[5vw] lg:!w-[4vw] lg:!h-[4vw]" />
           </span>
         </Link>
       </div>
